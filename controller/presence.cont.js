9d9c08c35d7675748ec9f51f30cd9f0a18aa5b24
oreo <bcd@localhost.localdomain>
2022-02-24 17:37:33 +0700

    rampung

--- controller/presence.cont.js
+++ controller/presence.cont.js
@@ -9,25 +9,44 @@ module.exports = {
 
 		let { id, date, info, description, morning } = req.body;
 		if (id && date && info && description && morning) {
-			db.findByIdAndUpdate(id,
-				{
-					$push: {
-						presences: {
-							date: date,
-							info: info,
-							description: description,
-							morning: morning,
+			let qFormated = new Date(date).toISOString();
+			qFormated = qFormated.split("T")[0];
+			var existed;
+			db.findOne({_id: id})
+			.then(data => {
+				existed = Object.values(data.presences).find((obj) => {
+					let oFormated = new Date(obj.date).toISOString();
+					oFormated = oFormated.split("T")[0];
+					return oFormated == qFormated && obj.morning == Boolean(morning);
+				})
+			})
+			.catch(err => {
+				res.status(500).json({message: "terjadi kesalahan."});
+			})
+			if(!existed){
+				db.findByIdAndUpdate(id,
+					{
+						$push: {
+							presences: {
+								date: date,
+								info: info,
+								description: description,
+								morning: morning,
+							}
 						}
+					}, (err, docs) => {
+						if (err) res.status(500).json({ message: "terjadi masalah." });
+						if (docs)
+							res.status(201).json({
+								message: "absensi berhasil.",
+								data: docs
+							});
 					}
-				}, (err, docs) => {
-					if (err) res.status(500).json({ message: "terjadi masalah." });
-					if (docs)
-						res.status(201).json({
-							message: "absensi berhasil.",
-							data: docs
-						});
-				}
-			)
+				)
+			}else {
+				res.status(500).json({message: "absen sudah terisi."});
+			}
+			
 		}
 		else res.status(500).json({ message: "form tidak boleh kosong." });
 	},
@@ -75,36 +94,41 @@ module.exports = {
 					if (err) res.status(500).json({ message: "terjadi masalah." });
 					if (data) {
 						var result = [];
-						data.disciples.forEach((value, index) => {
-							value.presences.forEach((values, indexs) => {
-								let qFormated = new Date(date).toISOString();
-								let oFormated = new Date(values.date).toISOString();
-								qFormated = qFormated.split("T")[0];
-								oFormated = oFormated.split("T")[0];
-								if (oFormated === qFormated) {
-									result.push({
-										id: value.id,
-										username: value.username,
-										presences: values
-									});
-								}else {
-									result.push({
-										id: value.id,
-										username: value.username,
-										presences: {
-											info: "",
-											_id: new mongoose.Types.ObjectId(),
-											date: oFormated,
-											description: ""
-										}
-									});
-								}
-							});
-						});
-						res.status(201).json({ message: result });
-					}
-				})
+						var morningTime = [true, false];
+						data.disciples.forEach(value => {
+							for(let i=0; i<2; i++){
+								result.push({
+									id: value._id,
+									username: value.username,
+									presences: {
+										_id: new mongoose.Types.ObjectId(),
+										morning: morningTime[i],
+										info: "",
+										description: "",
+										date: date,
+									}
+								})
+							}
+							value.presences.forEach(values => {
+								qFormated = new Date(date).toISOString()
+								oFormated = new Date(values.date).toISOString()
+								qFormated = qFormated.split('T')[0]
+								oFormated = oFormated.split('T')[0]
 
+								if(qFormated === oFormated){
+									result.forEach(rst => {
+										if(values.morning == rst.presences.morning) {
+											rst.presences = values
+										}
+									})
+								}
+							})
+							
+						})
+						
+					}res.json(result)
+					
+				})
 		} else res.status(500).json({ message: "form tidak boleh kosong" });
 	},
 }
